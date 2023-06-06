b7dbec5df2293781d501a979cbc4e78da682a70c
Déborah Dobles <126917588+deborahdobles@users.noreply.github.com>
2023-06-06 07:22:10 +0000

    listo (ojalá)

--- src/models.py
+++ src/models.py
@@ -5,28 +5,46 @@ from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
+
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    name = Column(String(80), nullable=False)
+    email = Column(String(80), nullable=False)
+    password = Column(String(250), nullable=False)
+    id_favorites= Column(Integer, ForeignKey('favorites.id'))
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Favorites(Base):
+    __tablename__ = 'favorites'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    id_planet = Column(Integer, ForeignKey('planets.id'))
+    name_planet = Column(String(250), nullable=False)
+    id_character = Column(Integer, ForeignKey('characters.id'))
+    name_character = Column(String(250), nullable=False)
+
+class Characters(Base):
+    __tablename__ = 'characters'
+    id = Column(Integer, primary_key=True)
+    name_character = Column(String(250), nullable=False)
+    gender = Column(String(250), nullable=False)
+    character_name = Column(String(250), nullable=False)
+    age = Column(String(250), nullable=False)
+    favorites = relationship(Favorites)
+
+class Planets(Base):
+    __tablename__ = 'planets'
+    id = Column(Integer, primary_key=True)
+    name_planet = Column(String(250), nullable=False)
+    terrain = Column(String(250), nullable=False)
+    climate = Column(String(250), nullable=False)
+    size = Column(Integer, nullable=False)
+    favorites = relationship(Favorites)
 
     def to_dict(self):
         return {}
 
-## Draw from SQLAlchemy base
+# Dibujar el diagrama a partir de la base de datos de SQLAlchemy
 render_er(Base, 'diagram.png')
+## Draw from SQLAlchemy base
